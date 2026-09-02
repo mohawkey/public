c093a99c0be75b3834f0f3957b257c8b68b9fae4
mohawkey <68987658+mohawkey@users.noreply.github.com>
2026-09-02 14:30:46 +0200

    Update install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-sudo apt update && apt-get upgrade -y
-sudo apt install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+# --------------------------------------------------------------------------------
+# Update System
+# --------------------------------------------------------------------------------
 
+sudo apt update
+sudo apt-get upgrade -y
+
+# --------------------------------------------------------------------------------
+# Install Certificates
+# --------------------------------------------------------------------------------
+
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# --------------------------------------------------------------------------------
 # Add the repository to Apt sources:
-echo \
+# --------------------------------------------------------------------------------
+
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
+# --------------------------------------------------------------------------------
+# Install Docker
+# --------------------------------------------------------------------------------
+
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
