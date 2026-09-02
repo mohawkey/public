b08e63ec0b1cc49ce3bcb477018fd0c1b01d5de5
mohawkey <68987658+mohawkey@users.noreply.github.com>
2026-09-02 14:37:15 +0200

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASE_URL="https://raw.githubusercontent.com/mohawkey/public/refs/heads/main/bin/"
+
+download_script() {
+    local script_name="$1"
+    local url="${BASE_URL}${script_name}"
+
+    read -r -p "Do you want to download ${script_name}? [y/N]: " answer
+
+    case "$answer" in
+        [yY]|[yY][eE][sS])
+            echo "Downloading ${script_name}..."
+            wget "$url"
+            ;;
+        *)
+            echo "Skipping ${script_name}."
+            ;;
+    esac
+}
+
+download_script "install-docker.sh"
