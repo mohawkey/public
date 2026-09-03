5b8c4006186877fb2f3a7fccdf45b9eaa24a4dcc
mohawkey <68987658+mohawkey@users.noreply.github.com>
2026-09-03 15:43:41 +0200

    Update install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -29,4 +29,5 @@ sudo echo \
 # Install Docker
 # --------------------------------------------------------------------------------
 
+sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
